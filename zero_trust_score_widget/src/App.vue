66b07411f18b699e5525e83bbd8eb45c475b0736
TziviMaklev <tm0527171162@gmail.com>
2024-10-15 23:19:24 +0300

    1

---
+++ zero_trust_score_widget/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ZeroTrustScoreWidget :data="jsonData" />
+  </div>
+</template>
+
+<script>
+import ZeroTrustScoreWidget from './components/ZeroTrustScoreWidget.vue'
+import jsonData from '../data.json' // Replace with your JSON data path
+
+  export default {
+    components: {
+      ZeroTrustScoreWidget,
+    },
+    data() {
+      return {
+        jsonData,
+      }
+    },
+  }
+</script>
